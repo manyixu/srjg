ff32b9981a51cc8eacf2d1eebe01b4bb0fbf15b4
geekyhmb@gmail.com <geekyhmb@gmail.com@52798df9-46d3-83a4-854e-65d7eb5731dc>
2011-12-02 10:37:40 +0000

    variables path instead of hard path -s condition to detect empty file $PreviousMovieList

--- branches/SqlExperimental/new_jukebox_update.sh
+++ branches/SqlExperimental/new_jukebox_update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 # Simple RSS Jukebox Generator
 
 
@@ -17,7 +17,7 @@ sed '1d;$d;s:<\(.*\)>\(.*\)</.*>:\1=\2:' ${CfgFile} >/tmp/Jukebox.cfg
 MoviesList="/tmp/movies.list"
 InsertList="/tmp/insert.list"
 DeleteList="/tmp/delete.list"
-PreviousMovieList="$Jukebox_Path/prevmovies.list"
+PreviousMovieList="${Jukebox_Path}/prevmovies.list"
 DiffList="/tmp/diff.list"
 IMDB=""
 Force_DB_Update=""
@@ -37,7 +37,7 @@ OPTIONS:
            string(s) will be skipped.  Strings must be separated by a ","
            (Optional) ex: -f sample,trailer
    -g      Generate moviesheets, thumbnails and NFO files. (Optional)
-           Please refer to $Jukebox_Path/imdb.sh for additional settings.
+           Please refer to ${Jukebox_Path}/imdb.sh for additional settings.
 NOTES:  If any of the arguments have spaces in them they must be surrounded by quotes: ""
     
 EOF
@@ -87,7 +87,8 @@ CreateMovieDB()
 # correct date which can be trivial with some players.
 {
 echo "Creating Database..."
-$Jukebox_Path/sqlite3 ${Movies_Path}movies.db "create table t1 (Movie_ID INTEGER PRIMARY KEY AUTOINCREMENT,head TEXT,genre TEXT,title TEXT,year TEXT,poster TEXT,info TEXT,file TEXT,footer TEXT,dateStamp DATE DEFAULT CURRENT_DATE);"
+${Jukebox_Path}/sqlite3 ${Movies_Path}movies.db \
+   "create table t1 (Movie_ID INTEGER PRIMARY KEY AUTOINCREMENT,head TEXT,genre TEXT,title TEXT,year TEXT,poster TEXT,info TEXT,file TEXT,footer TEXT,dateStamp DATE DEFAULT CURRENT_DATE);"
 }
 
 Force_DB_Creation()
@@ -107,7 +108,9 @@ GenerateMovieList()
 # Replace the comma in Movie_Filter to pipes |
 Movie_Filter=`echo $Movie_Filter | sed 's/,/|/ g'`
 echo "Searching for movies.."
-find "$Movies_Path" | egrep -i '\.(asf|avi|dat|divx|flv|img|iso|m1v|m2p|m2t|m2ts|m2v|m4v|mkv|mov|mp4|mpg|mts|qt|rm|rmp4|rmvb|tp|trp|ts|vob|wmv)$' | egrep -iv "$Movie_Filter" > $MoviesList
+find "$Movies_Path" \
+  | egrep -i '\.(asf|avi|dat|divx|flv|img|iso|m1v|m2p|m2t|m2ts|m2v|m4v|mkv|mov|mp4|mpg|mts|qt|rm|rmp4|rmvb|tp|trp|ts|vob|wmv)$' \
+  | egrep -iv "$Movie_Filter" > $MoviesList
 echo "Found `sed -n '$=' $MoviesList` movies"
 }
 
@@ -117,14 +120,14 @@ Infoparsing()
 {
 # Look for lines matching <title>
 while read LINE
-   do
-     # Strip out <title> to make it shorter.
-     SHORT="${LINE#<title>}"
-     # If it's not shorter, it didn't have <title>
-     [ "${#SHORT}" = "${#LINE}" ] && continue
-     MOVIETITLE="$SHORT"
-     break   # Found <title>, quit looking
-   done <"$MOVIEPATH/$INFONAME"
+do
+  # Strip out <title> to make it shorter.
+  SHORT="${LINE#<title>}"
+  # If it's not shorter, it didn't have <title>
+  if [ "${#SHORT}" = "${#LINE}" ]; then continue ; fi
+  MOVIETITLE="$SHORT"
+  break   # Found <title>, quit looking
+done <"$MOVIEPATH/$INFONAME"
         
 GENRE=`sed -e '/<genre/,/\/genre>/!d;/genre>/d' "$MOVIEPATH/$INFONAME"`
 MovieYear=`sed '/<year/!d;s:.*>\(.*\)</.*:\1:' "$MOVIEPATH/$INFONAME"`            
@@ -135,7 +138,7 @@ GenerateInsDelFiles()
 # Generate insertion and deletion files
 {
 sed -i -e 's/\[/\&lsqb;/g' -e 's/\]/\&rsqb;/g' $MoviesList # Conversion of [] for grep
-if [ -f "$PreviousMovieList" ] ; then
+if [ -s "$PreviousMovieList" ] ; then # because the grep -f don't work with empty file
   grep -vf $MoviesList $PreviousMovieList | sed -e 's/\&lsqb;/\[/g' -e 's/\&rsqb;/\]/g' > $DeleteList
   grep -vf $PreviousMovieList $MoviesList | sed -e 's/\&lsqb;/\[/g' -e 's/\&rsqb;/\]/g' > $InsertList
 else
@@ -158,30 +161,30 @@ do
 
    # Initialize defaults, replace later
    MOVIETITLE="$MOVIENAME</title>"
-   MOVIESHEET=/usr/local/etc/srjg/NoMovieinfo.bmp
-   MOVIEPOSTER=/usr/local/etc/srjg/nofolder.bmp
+   MOVIESHEET=${Jukebox_Path}/NoMovieinfo.bmp
+   MOVIEPOSTER=${Jukebox_Path}/nofolder.bmp
    GENRE=Unknown
    MovieYear=Unknown
 		
-   [ -e "$MOVIEPATH/$MOVIENAME.nfo" ] && INFONAME=$MOVIENAME.nfo;
-   [ -e "$MOVIEPATH/MovieInfo.nfo" ] && INFONAME=MovieInfo.nfo;
+   [ -e "$MOVIEPATH/$MOVIENAME.nfo" ] && INFONAME=$MOVIENAME.nfo
+   [ -e "$MOVIEPATH/MovieInfo.nfo" ] && INFONAME=MovieInfo.nfo
         
-   if [ -e "$MOVIEPATH/$INFONAME" ]; then Infoparsing; fi
+   [ -e "$MOVIEPATH/$INFONAME" ] && Infoparsing
 
    # Check for any files of known purpose inside the movie's folder.
    for FILE in "folder.jpg" "${MOVIENAME}.jpg"
-     do
-      [ ! -e "$MOVIEPATH/$FILE" ] && continue
-      MOVIEPOSTER="$MOVIEPATH/$FILE"
-      break
-     done
+   do
+    [ ! -e "$MOVIEPATH/$FILE" ] && continue
+    MOVIEPOSTER="$MOVIEPATH/$FILE"
+    break
+   done
 
    for FILE in "about.jpg" "0001.jpg" "${MOVIENAME}_sheet.jpg"
-     do
-      [ ! -e "$MOVIEPATH/$FILE" ] && continue
-      MOVIESHEET="$MOVIEPATH/$FILE"
-      break
-     done
+   do
+    [ ! -e "$MOVIEPATH/$FILE" ] && continue
+    MOVIESHEET="$MOVIEPATH/$FILE"
+    break
+   done
 
 dbgenre=$GENRE 
 dbtitle=`echo "<title>$MOVIETITLE" | sed "s/'/''/g"`
@@ -190,7 +193,9 @@ dbinfo=`echo "<info>$MOVIESHEET</info>" | sed "s/'/''/g"`
 dbfile=`echo "<file>$MOVIEPATH/$MOVIEFILE</file>" | sed "s/'/''/g"`
 dbYear=$MovieYear
 
-$Jukebox_Path/sqlite3 ${Movies_Path}movies.db "insert into t1 (head,genre,title,year,poster,info,file,footer) values ('<item>','$dbgenre','$dbtitle','$dbYear','$dbposter','$dbinfo','$dbfile','</item>');";
+${Jukebox_Path}/sqlite3 ${Movies_Path}movies.db \
+  "insert into t1 (head,genre,title,year,poster,info,file,footer) \
+  values ('<item>','$dbgenre','$dbtitle','$dbYear','$dbposter','$dbinfo','$dbfile','</item>');";
 
 done < $InsertList
 }
@@ -202,23 +207,23 @@ DBMovieDelete()
 echo "Removing movies from the Database ...."
 while read LINE
 do
-$Jukebox_Path/sqlite3 ${Movies_Path}movies.db  "DELETE from t1 WHERE file='<file>${LINE}</file>'";
+  ${Jukebox_Path}/sqlite3 ${Movies_Path}movies.db  "DELETE from t1 WHERE file='<file>${LINE}</file>'";
 done < $DeleteList
-$Jukebox_Path/sqlite3 ${Movies_Path}movies.db  "VACUUM";
+${Jukebox_Path}/sqlite3 ${Movies_Path}movies.db  "VACUUM";
 }
 
 
 #*****************  Main Program  *****************************************
 
 GenerateMovieList;
-[ -n "$IMDB" ] && $Jukebox_Path/imdb.sh;
-[ -n "$Force_DB_Update" ] && Force_DB_Creation;
-[ ! -f "${Movies_Path}movies.db" ] && CreateMovieDB;
+[ -n "$IMDB" ] &&  ${Jukebox_Path}/imdb.sh
+[ -n "$Force_DB_Update" ] && Force_DB_Creation
+[ ! -f "${Movies_Path}movies.db" ] && CreateMovieDB
 echo Indexing $Movies_Path;
 # if full update required then just delete (rm) $PreviousMovieList
 GenerateInsDelFiles;
-if [[ -s $DeleteList ]] ; then DBMovieDelete; fi
-if [[ -s $InsertList ]] ; then DBMovieInsert; fi
+[[ -s $DeleteList ]] && DBMovieDelete
+[[ -s $InsertList ]] && DBMovieInsert
 echo -e "\nDone!"
 
 
