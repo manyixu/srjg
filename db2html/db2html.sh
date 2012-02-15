6a60918e1f1901126304529bc043f7cacbc0fa88
mika.hellmann <mika.hellmann@52798df9-46d3-83a4-854e-65d7eb5731dc>
2012-02-15 22:18:29 +0000

    Started fixing db2html after recent changes in DB structure (not finished yet). Background on horizontal scrolling is fixed (reported by Georges).

--- db2html/db2html.sh
+++ db2html/db2html.sh
@@ -7,44 +7,50 @@
 # Paths
 SRJGPATH="`sed '/<Jukebox_Path/!d;s:.*>\(.*\)</.*:\1:' /usr/local/etc/srjg.cfg | grep "[!-~]"`"
 MOVPATH="`sed '/<Movies_Path/!d;s:.*>\(.*\)</.*:\1:' /usr/local/etc/srjg.cfg | grep "[!-~]"`"
-DB2HTML="$SRJGPATH/db2html"		# Main path to db2html
-DBFILE="$MOVPATH/SRJG/movies.db"	# Path to movies.db
-SQLPATH="$SRJGPATH/sqlite3"		# Path to sqlite binary
-DEFPOSTER="$SRJGPATH/nofolder.jpg"
-DEFSHEET="$SRJGPATH/NoMovieinfo.jpg"
-TMPTITLE="/tmp/srjg_title"
-TMPPOSTER="/tmp/srjg_poster"
-TMPSHEET="/tmp/srjg_sheet"
-TMPFILE="/tmp/srjg_file"
+SINGLEDB="`sed '/<SingleDb/!d;s:.*>\(.*\)</.*:\1:' /usr/local/etc/srjg.cfg | grep "[!-~]"`"
+SRJGPATH=$(echo ${SRJGPATH%\/})
 
-COLUMNS=10						# Number of columns to be created
-WIDTH=85						# Width of poster to be displayed
-HEIGHT=128						# Height of poster to be displayed
+DB2HTML="$SRJGPATH/db2html";		# Main path to db2html
+SQLPATH="$SRJGPATH/sqlite3";		# Path to sqlite binary
+DEFPOSTER="nofolder.jpg"
+DEFSHEET="NoMovieinfo.jpg"
+TMPPATH="/tmp/db2html_path"
+TMPHDD="/tmp/db2html_hdd"
+TMPTITLE="/tmp/db2html_title"
+TMPPOSTER="/tmp/db2html_poster"
+TMPSHEET="/tmp/db2html_sheet"
+TMPFILE="/tmp/db2html_file"
+TMPEXT="/tmp/db2html_ext"
+
+COLUMNS=10;						# Number of columns to be created
+WIDTH=85;						# Width of poster to be displayed
+HEIGHT=128;						# Height of poster to be displayed
 IDX=1;							# Starting index
 COUNT=1;						# Starting counter
 
-USECATEG=0						# Create categories (default Off)
-USEDATE=0						# Display creation date (default Off)
-USESHEET=0						# Include moviesheets (default Off)
-USETITLE=0						# Show title (default Off)
+USECATEG=0;						# Create categories (default Off)
+USEDATE=0;						# Display creation date (default Off)
+USEPOSTER=0;					# Include posters (default Off)
+USESYMLINK=0;					# Use symlinks (default Off)
+USETITLE=0;						# Show title (default Off)
 
 usage()
 {
 cat << EOF
 Usage: $0 options
-Example: $0 -o /home/JukeboxHTML/ -c -d -m -t -p tar
+Example: $0 -o /home/JukeboxHTML/ -a -d -m -s -t -c tar
 
 This script converts SRJG movies.db to HTML jukebox. 
 
 OPTIONS:
+   -a	   Create alphabetical page categories (Optional, no argument needed)
+   -c      Compress jukebox to archive: [tar] (Optional)
+   -d	   Display creation date (Optional, no argument needed)
    -h      Show this message
-   -o      Output path to HTML jukebox (Optional) 
-   -p      Type of archive: [tar] (Optional)
-   -c	   Create alphabetical page categories (Optional, no argument needed)
-   -d	   Generate creation date (Optional, no argument needed)
    -m	   Include moviesheets (Optional, no argument needed)
-   -t	   Enable displaying movie title (Optional, no argument needed)
-   
+   -o      Output path to HTML jukebox (Optional) 
+   -s	   Use symlinks (Optional, no argument needed)
+   -t	   Display movie title (Optional, no argument needed)
    
 NOTES:  If any of the path arguments have spaces in them they must be surrounded by quotes: ""
         Output path has to be ending with slash: /
@@ -52,18 +58,20 @@ EOF
 exit 1
 }
 
-while getopts o:p:cdmth OPTION 
+while getopts o:c:adhmst OPTION 
 do
   case $OPTION in  
 	 o)	MAINPATH=$OPTARG	;;
-     p)	PACK=$OPTARG		;;
-     c)	USECATEG=1			;;  	   
-     d)	USEDATE=1			;;  
-     m)	USESHEET=1			;;
-     t)	USETITLE=1			;;
+     c)	PACK=$OPTARG		;;
+     a)	USECATEG=1			;;
+     d)	USEDATE=1			;;
      h)	usage				;;
+     m)	USESHEET=1			;;
+	 s) USESYMLINK=1		;;
+     t)	USETITLE=1			;;
    esac
 done
+
 
 
 # If output path to HTML jukebox has NOT been provided, use main path from movies.db
@@ -73,8 +81,10 @@ then
 fi
 
 # Initialize title and paths
-HTMLPATH="$MAINPATH"SRJG
-IMGPATH="$HTMLPATH"/images
+HTMLPATH="$MAINPATH/SRJG"
+IMGPATH="$MAINPATH/SRJG/images"
+echo "Starting jukebox generation in $MAINPATH.."
+
 
 # If image path doesn't exist, create folder for HTML jukebox
 if [ ! -d "$IMGPATH" ];
@@ -84,12 +94,16 @@ then
 fi
 
 # Create symlink to HTML jukebox
+echo "Creating basic symlinks.."
 mount -o remount,rw /
 rm -f /tmp/www/srjg
 rm -f /tmp_orig/www/srjg
+rm -f /tmp/www/hdd
+rm -f /tmp_orig/www/hdd
 ln -sf "$HTMLPATH" /tmp/www/srjg
 ln -sf "$HTMLPATH" /tmp_orig/www/srjg
-  
+ln -sf "$MOVPATH" /tmp/www/hdd
+ln -sf "$MOVPATH" /tmp_orig/www/hdd
   
 # Default template header
 echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -99,18 +113,9 @@ echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <link rel="StyleSheet" type="text/css" href="s.css">
 </head>
 
-<body onloadset="nextlink" focuscolor="#FDDB2B" focustext="#000000" bgproperties="fixed" style="background-position: center top; background-repeat: no-repeat;" background="images/b.jpg" bgcolor="#000000">
+<body>
 <table align="center" width="1100"><tbody><tr><td>
 <div class="title" align="center"><font color="#colortitle">Jukebox</font></div>' > "$HTMLPATH/index.html"
-
-# Creating /hdd symlink
-if [ $USETITLE = 1 ];
-then
-  rm -f /tmp/www/hdd
-  rm -f /tmp_orig/www/hdd
-  ln -sf "$MOVPATH" /tmp/www/hdd
-  ln -sf "$MOVPATH" /tmp_orig/www/hdd
-fi
 
 if [ $USEDATE = 1 ];
 then
@@ -132,16 +137,32 @@ fi
   	    
 echo '<br><br><table align="center" cellpadding="1"><tbody><tr>' >> "$HTMLPATH/index.html"
 
-# Get all titles and links
+
+# Path to movies.db
+if [ $SINGLEDB = "yes" ];
+then
+	DBFILE="$SRJGPATH/movies.db"
+else
+	DBFILE="$MOVPATH/SRJG/movies.db"	
+fi
+
+
+# Get all required data
+"$SQLPATH" -separator '' "$DBFILE" "SELECT path FROM t1 ORDER BY title COLLATE NOCASE" | sed -n '/<path>/,/<\/path>/s/.*<path>\(.*\)<\/path>/\1/p' > "$TMPPATH"
+
+cat "$TMPPATH" | sed "s|$MOVPATH|/hdd|" > "$TMPHDD"
+
 "$SQLPATH" -separator '' "$DBFILE" "SELECT title FROM t1 ORDER BY title COLLATE NOCASE" | sed -n '/<title>/,/<\/title>/s/.*<title>\(.*\)<\/title>/\1/p' > "$TMPTITLE"
 
 "$SQLPATH" -separator '' "$DBFILE" "SELECT poster FROM t1 ORDER BY title COLLATE NOCASE" | sed -n '/<poster>/,/<\/poster>/s/.*<poster>\(.*\)<\/poster>/\1/p' > "$TMPPOSTER"
 
 "$SQLPATH" -separator '' "$DBFILE" "SELECT info FROM t1 ORDER BY title COLLATE NOCASE" | sed -n '/<info>/,/<\/info>/s/.*<info>\(.*\)<\/info>/\1/p' > "$TMPSHEET"
 
-"$SQLPATH" -separator '' "$DBFILE" "SELECT file FROM t1 ORDER BY title COLLATE NOCASE" | sed -n '/<file>/,/<\/file>/s/.*<file>\(.*\)<\/file>/\1/p' | sed "s|$MOVPATH|/hdd/|" > "$TMPFILE"
+"$SQLPATH" -separator '' "$DBFILE" "SELECT file FROM t1 ORDER BY title COLLATE NOCASE" | sed -n '/<file>/,/<\/file>/s/.*<file>\(.*\)<\/file>/\1/p' > "$TMPFILE"
 
-echo "Movies found: `sed -n '$=' "$TMPTITLE"`"  
+"$SQLPATH" -separator '' "$DBFILE" "SELECT ext FROM t1 ORDER BY title COLLATE NOCASE" | sed -n '/<ext>/,/<\/ext>/s/.*<ext>\(.*\)<\/ext>/\1/p' > "$TMPEXT"
+
+echo "Movies found: `sed -n '$=' "$TMPTITLE"`"
 
 # Copy basic files
 echo "Copying basic files.."
@@ -152,34 +173,19 @@ cp "$DB2HTML/s.css" "$HTMLPATH"
 while read MOVIE
 do
   echo "Processing $IDX: $MOVIE.."
+  MPATH="`sed -n "$IDX"p "$TMPPATH"`"
+  MPOST="`sed -n "$IDX"p "$TMPPOSTER"`"
   
-  POSTER="`sed -n "$IDX"p "$TMPPOSTER"`"
-  if [ "$POSTER" != "$DEFPOSTER" ];
+  if [ "$MPOST" != "$DEFPOSTER" ];
   then
-	cp "$POSTER" "$IMGPATH/p$IDX.jpg" 
+	echo "Copying poster.."
+	cp "$MPATH/$MPOST" "$IMGPATH/p$IDX.jpg" 
 	SHOWPOSTER="images/p$IDX.jpg"
   else
     SHOWPOSTER="images/defposter.jpg"
   fi 
-
-  
-  if [ $USESHEET = 1 ];
-  then
-	SHEET="`sed -n "$IDX"p "$TMPSHEET"`"
-	
-	if [ "$SHEET" != "$DEFSHEET" ];
-	then
-	  cp "$SHEET" "$IMGPATH/m$IDX.jpg"
-	  SHOWSHEET="images/m$IDX.jpg"
-	else
-      SHOWSHEET="images/defsheet.jpg"
-	fi
-	
-  else
-    SHOWSHEET="#"
-  fi
-  
  
+  
   if [ $USETITLE = 1 ];
   then
     TITLE="`sed -n "$IDX"p "$TMPFILE"`"
@@ -198,7 +204,7 @@ do
   fi
   
   echo '<td valign="top" width="'"$WIDTH"'" height="'"$HEIGHT"'"><center><a href="'"$SHOWSHEET"'" title="'"$MOVIE"'" '"$SHOWCATEG"' id="thumbimage"><img src="'"$SHOWPOSTER"'" width="'"$WIDTH"'" height="'"$HEIGHT"'"></a>'"$SHOWTITLE"'</center></td>' >> "$HTMLPATH/index.html"
-
+  
   if [ $COUNT != $COLUMNS ];
   then
     let COUNT=$COUNT+1;
@@ -207,7 +213,7 @@ do
     echo '</tr><tr>' >> "$HTMLPATH/index.html"
   fi
   
-  let IDX=$IDX+1 
+  let IDX=$IDX+1;
 done < "$TMPTITLE"
 
 
@@ -219,6 +225,6 @@ echo "Jukebox saved into $HTMLPATH/"
 # Pack everything
 if [ "$PACK" == "tar" ];
 then
-  echo "Packing into "$MAINPATH"html.tar.."
-  tar -c -f "$MAINPATH"html.tar -C "$MAINPATH" HTML/
+  echo "Packing into $MAINPATH/html.tar.."
+  tar -c -f "$MAINPATH"/html.tar -C "$MAINPATH" HTML/
 fi
